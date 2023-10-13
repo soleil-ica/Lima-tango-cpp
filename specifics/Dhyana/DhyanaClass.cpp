7a7813ac6caf3af89571a179a04fecae12e1f38f
Marc Desgranges <126486372+mdesgranges@users.noreply.github.com>
2023-10-13 10:08:34 +0200

    [ICATHALES-554] dhyana95 TemperatureTargetAtInit  (#82)
    * [ICATHALES-554] code review + no longer enter fault state at write_at_init
    
    * [ICATHALES-552] write_at_init does not set the device at FAULT

--- specifics/Dhyana/DhyanaClass.cpp
+++ specifics/Dhyana/DhyanaClass.cpp
@@ -305,6 +305,20 @@ void DhyanaClass::set_default_property()
 	}
 	else
 		add_wiz_dev_prop(prop_name, prop_desc);
+	prop_name = "TemperatureTargetAtInit";
+	prop_desc = "TemperatureTarget at init (default is 15)";
+	prop_def  = "15";
+	vect_data.clear();
+	vect_data.push_back("15");
+	if (prop_def.length()>0)
+	{
+		Tango::DbDatum	data(prop_name);
+		data << vect_data ;
+		dev_def_prop.push_back(data);
+		add_wiz_dev_prop(prop_name, prop_desc,  prop_def);
+	}
+	else
+		add_wiz_dev_prop(prop_name, prop_desc);
 }
 
 //--------------------------------------------------------
