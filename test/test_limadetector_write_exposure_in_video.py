6d0c7de66132d42dc02b0fb1d21eb766eaf7d0d6
Arafat Noureddine <arafat.noureddine@synchrotron-soleil.fr>
2026-04-09 16:07:24 +0200

    Migration test\limadetector_write_exposure_in_video.py to python 3 and enhance incrementation strategy

--- test/test_limadetector_write_exposure_in_video.py
+++ test/test_limadetector_write_exposure_in_video.py
@@ -1,82 +1,124 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #########################################################
-#Arafat NOUREDDINE
-#2014/12/03
-#Purpose : Test writing exposure attribute while video acquisition is already RUNNING
+# Arafat NOUREDDINE
+# 2014/12/03
+# Purpose : Test writing exposure attribute while video acquisition is already RUNNING
 #########################################################
+
 import os
 import sys
 import PyTango
 import time
 import datetime
 
-proxy = ''
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # build exception
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 class BuildError(Exception):
-  pass
+    pass
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # run
-#------------------------------------------------------------------------------
-def run(proxy_name = 'arafat/lima_basler/basler.2', exposure_time = 100, write_period = 1, nb_loops = 1, exp_increment_value = 1):
-    # print arguments
-    print '\nProgram inputs :\n--------------'
-    print 'proxy_name\t = ', proxy_name
-    print 'exposure_time\t =  %s (ms)' %(exposure_time)
-    print 'write_period\t =  %s (ms)' %(write_period)
-    print 'nb_loops\t = ', nb_loops
-    print 'exp_increment_value\t = ', exp_increment_value
-    #Configure the device
-    print '\nConfigure the Device :\n--------------'
-    timeBegin = datetime.datetime.now()
-    print '\r',timeBegin.isoformat(), " - Create a DeviceProxy to Device ", proxy_name
+# ------------------------------------------------------------------------------
+def run(proxy_name='arafat/lima_basler/basler.2',
+        exposure_time=100,
+        exp_increment=1,
+        exposure_max=1000,
+        wait=1,
+        nb_loops=1):
+
+    print("\nProgram inputs :\n--------------")
+    print("proxy_name\t =", proxy_name)
+    print("exposure_time\t = {} (ms)".format(exposure_time))
+    print("exp_increment\t =", exp_increment)
+    print("exposure_max\t =", exposure_max)    
+    print("wait\t = {} (ms)".format(wait))
+    print("nb_loops\t =", nb_loops)
+
+
+    # Conversion en float/int une seule fois
+    exposure_time = float(exposure_time)
+    wait = float(wait)
+    nb_loops = int(nb_loops)
+    exp_increment = float(exp_increment)
+    exposure_max = float(exposure_max)
+
+    # Configure the device
+    print("\nConfigure the Device :\n--------------")
+
+    time_begin = datetime.datetime.now()
+    print("\r{} - Create a DeviceProxy to Device {}".format(time_begin.isoformat(), proxy_name))
+
     proxy = PyTango.DeviceProxy(proxy_name)
-    timeBegin = datetime.datetime.now()
-    print '\r',timeBegin.isoformat(), " - Ping proxy(\"",proxy_name,"\") ",proxy.ping()," us"
-    
-    timeBegin = datetime.datetime.now()
-    print '\r',timeBegin.isoformat(), " - State = ",proxy.state();
-    print '--> Stop()'
+
+    time_begin = datetime.datetime.now()
+    print("\r{} - Ping proxy(\"{}\") {} us".format(time_begin.isoformat(), proxy_name, proxy.ping()))
+
+    time_begin = datetime.datetime.now()
+    print("\r{} - State = {}".format(time_begin.isoformat(), proxy.state()))
+
+    print("--> Stop()")
     proxy.Stop()
-    timeBegin = datetime.datetime.now()
-    print '\r',timeBegin.isoformat(), " - State = ",proxy.state();
-    print '--> Start()'
+
+    time_begin = datetime.datetime.now()
+    print("\r{} - State = {}".format(time_begin.isoformat(), proxy.state()))
+
+    print("--> Start()")
     proxy.Start()
-    timeBegin = datetime.datetime.now()
-    print '\r',timeBegin.isoformat(), " - State = ",proxy.state();
-    nb_loops=int(nb_loops)
+
+    time_begin = datetime.datetime.now()
+    print("\r{} - State = {}".format(time_begin.isoformat(), proxy.state()))
+
     try:
         current_loop = 1
-	while(current_loop <= nb_loops):
-            print '\n============================'
-            print 'Loop : ', current_loop,
-            print '\n============================'
-            timeBegin = datetime.datetime.now().isoformat()
-            print '\r', timeBegin, "---> Writing exposure time = ", float(exposure_time)+float(int(current_loop)*float(exp_increment_value))
-            proxy.exposureTime = float(exposure_time)+float(int(current_loop)*float(exp_increment_value))
-            time.sleep(float(write_period)/1000.0)
-			while (proxy.currentFrame == 0)
-				time.sleep(float(1)/1000.0)
-			print proxy.image	
-            current_loop+=1
-			            
-            print '\noutput :\n--------------'
-    except Exception as err:
-	   sys.stderr.write('--------------\nERROR :\n--------------\n%s\n' %err)
+        current_exposure = exposure_time
 
-#------------------------------------------------------------------------------
+        while current_loop <= nb_loops:
+            print("\n============================")
+            print("Loop :", current_loop)
+            print("============================")
+
+            time_begin = datetime.datetime.now().isoformat()
+
+            print("\r{} ---> Writing exposure time = {}".format(time_begin, current_exposure))
+            proxy.exposureTime = current_exposure
+
+            time.sleep(wait / 1000.0)
+
+            # Wait until a frame is available
+            while proxy.currentFrame == 0:
+                time.sleep(1 / 1000.0)
+
+            print(proxy.image)
+
+            # Calcul de la prochaine valeur
+            current_exposure += exp_increment
+            if current_exposure >= exposure_max:
+                current_exposure = exposure_time
+
+            current_loop += 1
+
+        print("\noutput :\n--------------")
+
+    except Exception as err:
+        sys.stderr.write('--------------\nERROR :\n--------------\n{}\n'.format(err))
+
+
+# ------------------------------------------------------------------------------
 # Usage
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 def usage():
-  print "Usage: [python] test_limadetector_write_exposure_in_video.py <my/device/proxy> <exposureTime in ms> <writingPeriod in ms> <nb_loops> <exp_increment_value>"
-  sys.exit(1)
-  
-#------------------------------------------------------------------------------
+    print("Usage: test_limadetector_write_exposure_in_video.py "
+          "<my/device/proxy> "
+          "<exp_time in ms> <exp_increment> <exp_max in ms> <wait in ms> "
+          "<nb_loops>")
+    sys.exit(1)
+
+
+# ------------------------------------------------------------------------------
 # Main Entry point
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 if __name__ == "__main__":
-#    if len(sys.argv) != 6:
-#        usage()
-    print run(*sys.argv[1:])
+    if len(sys.argv) != 7:
+         usage()    
+    print(run(*sys.argv[1:]))
